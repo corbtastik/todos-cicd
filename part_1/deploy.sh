067ec92725bbd64548e9249d056db59cd2715d6e
Corbs <corbsmartin@pivotal.io>
2018-08-14 09:52:57 -0500

    one shot manifest for part 1

--- part_1/deploy.sh
+++ part_1/deploy.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-if [ -z "$1" ] && [ -z "$2" ]
+if [ -z "$1" ]
 then
     version=1.0.0.SNAP
 else
     version=$1
 fi
 cd ..
+source deploy.conf
 ./build.sh "todos-gateway" ${version}
 ./build.sh "todos-api" ${version}
-./deploy.sh "todos-gateway" ${version} "todos-gateway"
-./deploy.sh "todos-api" ${version} "todos-api"
-./deploy.sh "todos-ui" ${version} "todos-ui"
+./deploy.sh "todos-gateway" ${version} ${cf_todos_gateway_route}
+./deploy.sh "todos-api" ${version} ${cf_todos_api_route}
+./deploy.sh "todos-ui" ${version} ${cf_todos_ui_route}
 cd part_1
--- part_2/deploy.sh
+++ part_2/deploy.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-if [ -z "$1" ] && [ -z "$2" ]
+if [ -z "$1" ]
 then
     version=1.0.0.SNAP
 else
     version=$1
 fi
 cd ..
+source deploy.conf
 ./build.sh "config-server" ${version}
-# ./build.sh "cloud-index" ${version}
-./deploy.sh "config-server" ${version} "config-srv"
-# ./deploy.sh "cloud-index" ${version} "cloud-index"
+./build.sh "cloud-index" ${version}
+./deploy.sh "config-server" ${version} ${cf_config_server_route}
+./deploy.sh "cloud-index" ${version} ${cf_cloud_index_route}
 cd part_2
